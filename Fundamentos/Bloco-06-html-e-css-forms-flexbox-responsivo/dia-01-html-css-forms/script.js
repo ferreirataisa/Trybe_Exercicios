dc18309b863bec83007fcd4e6f878e5fa0daa749
Taisa Mattiazzi Ferreira <ferreirataisa@yahoo.com.br>
2022-04-28 14:34:00 -0300

    Exercicio estilizado

--- Fundamentos/Bloco-06-html-e-css-forms-flexbox-responsivo/dia-01-html-css-forms/script.js
+++ Fundamentos/Bloco-06-html-e-css-forms-flexbox-responsivo/dia-01-html-css-forms/script.js
@@ -2,19 +2,36 @@
 
 const botaoEnviar = document.getElementById('enviar');
 
-botaoEnviar.addEventListener('click', parar);
+botaoEnviar.addEventListener('click', enviar);
 
-function parar (event) {
+function enviar (event) {
     event.preventDefault();
 }
 
 // 4.Crie um botão que limpe as informações contidas nos campos;
 
-const botaoApagar = document.getElementById('apagar');
+// const botaoApagar = document.getElementById('apagar');
 
-botaoApagar.addEventListener('click', limpar);
+// botaoApagar.addEventListener('click', limpar);
 
-function limpar() {
-    form.reset();
+// function limpar() {
+//     // form.reset();
+//     // ref: https://www.w3schools.com/jsref/met_form_reset.asp
+    
+//     // ou
+    
+//     // const txtNome = document.getElementById('nome');
+//     // txtNome.value = ''
+//     // ou criar uma variavel para cada campo... e adicionar a string vazia!
+// }
+
+// Bônus
+// 1.A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+
+const autoriza = document.getElementById('concorda');
+
+if (autoriza.value == 'concordo') {
+    botaoEnviar.addEventListener('click', enviar);
 }
-// ref: https://www.w3schools.com/jsref/met_form_reset.asp
+
+// 2.Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.
