3379d5eebb36206dbac9bf5f5122e66b0869f55c
Taisa Mattiazzi Ferreira <ferreirataisa@yahoo.com.br>
2022-05-30 21:50:32 -0300

    Atualizo fundamentos

--- Fundamentos/Bloco-07-introducao-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-execao-e-objetos/script.js
+++ Fundamentos/Bloco-07-introducao-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-execao-e-objetos/script.js
@@ -1,20 +1,56 @@
-const customer1 = {
-    firstName: 'Roberto',
-    age: 22,
-    job: 'Teacher',
+const order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        }
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        }
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      }
+    },
+    payment: {
+      total: 60,
+    },
   };
   
-  console.log(customer1);
+const customerInfo = (order) => {
+    const fazerPedido = `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, telefone: ${order.phoneNumber}, R. ${order.address.street}, N° ${order.address.number}, AP: ${order.address.apartment}`;
+
+    console.log(fazerPedido);
+}
+
+  customerInfo(order);
   
-  customer1.lastName = 'Faria';
-  console.log(customer1);
+
+  const orderModifier = (order) => {
+    order.name = 'Luiz Silva';
+    order.payment = '50'; 
+
+    const retornoPedido = `Olá ${order.name}, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é de R$ ${order.payment},00.`;
+
+    console.log(retornoPedido);
   
-  const customer2 = {
-    firstName: 'Maria',
-    age: 23,
-    job: 'Medic',
-  };
+  }
   
-  console.log(customer2);
-  customer2['lastName'] = 'Silva';
-  console.log(customer2);
+  orderModifier(order);
